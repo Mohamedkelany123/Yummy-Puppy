75a996e21af2260b0c7924a547579d4397374db4
Karim Sobh <kmsobh@blnk.ai>
2023-12-10 18:11:40 +0200

    fix last setp of cluster setting_last_accrual_date 10/12/2023

--- sources/mains/cmd_services/new_lms.cpp
+++ sources/mains/cmd_services/new_lms.cpp
@@ -784,7 +784,7 @@ int main (int argc, char ** argv)
             (
                 // 7(WRITEOFF) and 14(PARTIAL-SETTLE-CHARGE-OFF) are not in django
                 new UnaryOperator ("loan_app_loan.status_id",nin,"6, 8, 12, 13, 15"),
-                new UnaryOperator ("loan_app_loan.lms_closure_status",eq,to_string(closure_status::LAST_ACCRUED_DAY-1)),
+                new UnaryOperator ("loan_app_loan.lms_closure_status",eq,to_string(closure_status::LAST_ACCRUED_DAY-1)),                
                 new OROperator (
                     new UnaryOperator ("new_lms_installmentextension.partial_accrual_date",lte,closure_date_string),
                     new ANDOperator (
@@ -801,7 +801,8 @@ int main (int argc, char ** argv)
                 BDate partial_accrual_date(ie_orm->get_partial_accrual_date());
                 BDate first_accrual_adjustment_date(lal_orm->get_first_accrual_adjustment_date());
                 BDate last_accrued_interest_day(lal_orm->get_last_accrued_interest_day());
-                BDate new_last_accrued_interest_day ("");
+                BDate new_last_accrued_interest_day (lal_orm->loan_booking_day());
+                new_last_accrued_interest_day.dec_day();
 
                 //Missing Conditions From Django --> last_accrued_interest_day_service.py --> line: [21-27]
                 //--------------------------------------------------------------------------------------------------------
@@ -814,11 +815,11 @@ int main (int argc, char ** argv)
                 }
                 //----------------------------------------------------------------------------------------------------------
 
-                if ( accrual_date() >= partial_accrual_date() &&  accrual_date() >= first_accrual_adjustment_date ()) 
+                if ( accrual_date() >= partial_accrual_date() &&  accrual_date() >= first_accrual_adjustment_date () && accrual_date() <= closure_date ()) 
                     new_last_accrued_interest_day.set_date(accrual_date.getDateString());
-                else if ( partial_accrual_date() >= accrual_date() &&  partial_accrual_date() >= first_accrual_adjustment_date ()) 
+                else if ( partial_accrual_date() >= first_accrual_adjustment_date () && partial_accrual_date() <= closure_date () ) 
                     new_last_accrued_interest_day.set_date(partial_accrual_date.getDateString());
-                else if ( first_accrual_adjustment_date() >= accrual_date() &&  first_accrual_adjustment_date() >= partial_accrual_date ()) 
+                else if ( first_accrual_adjustment_date() <= closure_date ()) 
                     new_last_accrued_interest_day.set_date(first_accrual_adjustment_date.getDateString());
 
                 if ( last_accrued_interest_day() <  new_last_accrued_interest_day())
