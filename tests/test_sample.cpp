b22548545f6d77cee961820b3c2ba3490159c3c9
Karim Sobh <kmsobh@blnk.ai>
2023-12-13 14:21:09 +0200

    Adding ORM generate Field Map feature + fixing some bugs 13/12/2023

--- tests/test_sample.cpp
+++ tests/test_sample.cpp
@@ -33,32 +33,51 @@ TEST(ClosureTest, InstallmentExtention) {
     
     // PSQLConnectionManager psqlConnectionManager;
     // psqlConnectionManager.addDataSource("main", "192.168.65.203", 5432, "7_full_closure_c", "postgres", "postgres");
-    psqlController.addDataSource("main", "192.168.65.203", 5432, "7_full_closure_c", "postgres", "postgres");
+    // psqlController.addDataSource("main", "192.168.65.203", 5432, "7_full_closure_c", "postgres", "postgres");
+    psqlController.addDataSource("main", "127.0.0.1", 5432, "django_ostaz_25102023", "postgres", "postgres");
 
-    PSQLJoinQueryIterator *  psqlQueryJoin = new PSQLJoinQueryIterator ("main",
-        {new new_lms_installmentextension_primitive_orm(),new loan_app_installment_primitive_orm(),new loan_app_loan_primitive_orm(),new crm_app_customer_primitive_orm()},
-        {{{"loan_app_installment","loan_id"},{"loan_app_loan","id"}},
-        {{"loan_app_installment","id"},{"new_lms_installmentextension","installment_ptr_id"}},
-        {{"crm_app_customer","id"},{"loan_app_loan","customer_id"}}
-        });
 
-        psqlQueryJoin->filter(
-            ANDOperator 
-            (
-                new UnaryOperator ("new_lms_installmentextension.is_marginalized",eq,"f"),
-                new UnaryOperator ("new_lms_installmentextension.payment_status",in,"0,4,5")
-            )
-        );
+    new_lms_installmentextension_primitive_orm_iterator * nliei_old = new new_lms_installmentextension_primitive_orm_iterator("main_old");
+    new_lms_installmentextension_primitive_orm_iterator * nliei_new = new new_lms_installmentextension_primitive_orm_iterator("main_new");
+    nliei_old->execute();
+    nliei_new->execute();
+    for ( ;  ; )
+    {
+        new_lms_installmentextension_primitive_orm * x_old = nliei_old->next();
+        new_lms_installmentextension_primitive_orm * x_new = nliei_new->next();
+        x_old->generateFieldsMap()
+        jsonb x_old->compare(x_new);
+        if (x == NULL ) break;
+        cout << x->get_installment_ptr_id() << endl;
 
-    // Process the results and print information
-    psqlQueryJoin->process (10,[](map <string,PSQLAbstractORM *> * orms,int partition_number,mutex * shared_lock) { 
-        new_lms_installmentextension_primitive_orm * ieorm = ORM(new_lms_installmentextension,orms);
-        loan_app_loan_primitive_orm * lal_orm = ORM(loan_app_loan,orms);
-        cout << "hiiiii" << endl;
-        });
-    cout << "C++ Results:" << psqlQueryJoin->getResultCount() << " record(s)" << endl;
+    }
+    delete (nliei);
 
-    delete (psqlQueryJoin);
+    // PSQLJoinQueryIterator *  psqlQueryJoin = new PSQLJoinQueryIterator ("main",
+    //     {new new_lms_installmentextension_primitive_orm(),new loan_app_installment_primitive_orm(),new loan_app_loan_primitive_orm(),new crm_app_customer_primitive_orm()},
+    //     {{{"loan_app_installment","loan_id"},{"loan_app_loan","id"}},
+    //     {{"loan_app_installment","id"},{"new_lms_installmentextension","installment_ptr_id"}},
+    //     {{"crm_app_customer","id"},{"loan_app_loan","customer_id"}}
+    //     });
+
+    //     psqlQueryJoin->filter(
+    //         ANDOperator 
+    //         (
+    //             new UnaryOperator ("new_lms_installmentextension.is_marginalized",eq,"f"),
+    //             // new UnaryOperator ("new_lms_installmentextension.payment_status",in,"0,4,5"),
+    //             new UnaryOperator ("new_lms_installmentextension.installment_ptr_id",lte,"1000")
+    //         )
+    //     );
+
+    // // Process the results and print information
+    // psqlQueryJoin->process (10,[](map <string,PSQLAbstractORM *> * orms,int partition_number,mutex * shared_lock) { 
+    //     new_lms_installmentextension_primitive_orm * ieorm = ORM(new_lms_installmentextension,orms);
+    //     loan_app_loan_primitive_orm * lal_orm = ORM(loan_app_loan,orms);
+    //     cout << "hiiiii" << endl;
+    //     });
+    // cout << "C++ Results:" << psqlQueryJoin->getResultCount() << " record(s)" << endl;
+
+    // delete (psqlQueryJoin);
 
 
     EXPECT_EQ(5, 5);
