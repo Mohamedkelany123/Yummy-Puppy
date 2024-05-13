4a3069b82957071bb074622a3c0436465f0b3229
Abdallah Gomaa <abdallah.gomaa@blnk.ai>
2024-05-13 13:21:04 +0300

    removed setters and getters from .h to .cpp

--- headers/blnk/TemplateManager.h
+++ headers/blnk/TemplateManager.h
@@ -35,45 +35,45 @@ class LedgerAmount: public x
         int account_id;
         bool is_credit;
     public:
-        LedgerAmount(){}
+        LedgerAmount();
         // Setters
-        void setId(int id) {this->id = id; }
-        void setName(string name) { this->name = name; }
-        void setDebitAccountId(int debit_account_id) { this->debit_account_id = debit_account_id; }
-        void setCashierId(int cashier_id) { this->cashier_id = cashier_id; }
-        void setCreditAccountId(int credit_account_id) { this->credit_account_id = credit_account_id; }
-        void setCustomerId(int customer_id) { this->customer_id = customer_id; }
-        void setLoanId(int loan_id) { this->loan_id = loan_id; }
-        void setInstallmentId(int installment_id) { this->installment_id = installment_id; }
-        void setMerchantId(int merchant_id) { this->merchant_id = merchant_id; }
-        void setBondId(int bond_id) {this->bond_id = bond_id; }
-        void setLatefeeId(int latefee_id) { this->latefee_id = latefee_id; }
-        void setLegId(int leg_id) { this->leg_id = leg_id; }
-        void setEntryId(int entry_id) { this->entry_id = entry_id; }
-        void setAmount(float amount) { this->amount = amount; }
-        void setAccountId(float account_id) { this->account_id = account_id; }
-        void setIsCredit(bool is_credit) {this->is_credit = is_credit; }
+        void setId(int id);
+        void setName(string name);
+        void setDebitAccountId(int debit_account_id);
+        void setCashierId(int cashier_id);
+        void setCreditAccountId(int credit_account_id);
+        void setCustomerId(int customer_id);
+        void setLoanId(int loan_id);
+        void setInstallmentId(int installment_id);
+        void setMerchantId(int merchant_id);
+        void setBondId(int bond_id);
+        void setLatefeeId(int latefee_id);
+        void setLegId(int leg_id);
+        void setEntryId(int entry_id);
+        void setAmount(float amount);
+        void setAccountId(float account_id);
+        void setIsCredit(bool is_credit);
 
 
 
         // Getters
-        int getId() { return id; }
-        string getName() { return name; }
-        int getDebitAccountId() { return debit_account_id; }
-        int getCashierId() { return cashier_id; }
-        int getCreditAccountId() { return credit_account_id; }
-        int getCustomerId() { return customer_id; }
-        int getLoanId() { return loan_id; }
-        int getInstallmentId() { return installment_id; }
-        int getMerchantId() { return merchant_id; }
-        int getBondId() { return bond_id; }
-        int getLatefeeId() { return latefee_id; }
-        int getLegId() { return leg_id; }
-        int getEntryId() { return entry_id; }
-        float getAmount() { return amount; }
-        int getAccountId() { return account_id; }
-        bool getIsCredit() { return is_credit; }
-        ~LedgerAmount(){}
+        int getId();
+        string getName();
+        int getDebitAccountId();
+        int getCashierId();
+        int getCreditAccountId();
+        int getCustomerId();
+        int getLoanId();
+        int getInstallmentId();
+        int getMerchantId();
+        int getBondId();
+        int getLatefeeId();
+        int getLegId();
+        int getEntryId();
+        float getAmount();
+        int getAccountId();
+        bool getIsCredit();
+        ~LedgerAmount();
 };
 
 class TemplateLeg
@@ -97,40 +97,40 @@ class TemplateLeg
         vector<int> debit_available_ids;
         // string variable_name;
     public:
-        TemplateLeg(){}
-        TemplateLeg(bool _debit){}
+        TemplateLeg();
+        TemplateLeg(bool _debit);
 
         //SETTERS
-        void setCashierIdRequired(bool cashier_id_required) { this->cashier_id_required = cashier_id_required; }
-        void setCreditAvailableIds(const vector<int>& credit_available_ids) { this->credit_available_ids = credit_available_ids; }
-        void setCreditBondIdRequired(int credit_bond_id_required) { this->credit_bond_id_required = credit_bond_id_required; }
-        void setCustomerIdRequired(bool customer_id_required) { this->customer_id_required = customer_id_required; }
-        void setDebitAvailableIds(const vector<int>& debit_available_ids) { this->debit_available_ids = debit_available_ids; }
-        void setDebitBondIdRequired(int debit_bond_id_required) { this->debit_bond_id_required = debit_bond_id_required; }
-        void setId(int id) { this->id = id; }
-        void setInstallmentIdRequired(bool installment_id_required) { this->installment_id_required = installment_id_required; }
-        void setLoanIdRequired(bool loan_id_required) { this->loan_id_required = loan_id_required; }
-        void setMerchantIdRequired(bool merchant_id_required) { this->merchant_id_required = merchant_id_required; }
-        void setName(const string& name) { this->name = name; }
-        void setLegRequired(bool leg_required) { this->leg_required = leg_required; }
+        void setCashierIdRequired(bool cashier_id_required);
+        void setCreditAvailableIds(const vector<int>& credit_available_ids);
+        void setCreditBondIdRequired(int credit_bond_id_required);
+        void setCustomerIdRequired(bool customer_id_required);
+        void setDebitAvailableIds(const vector<int>& debit_available_ids);
+        void setDebitBondIdRequired(int debit_bond_id_required);
+        void setId(int id);
+        void setInstallmentIdRequired(bool installment_id_required);
+        void setLoanIdRequired(bool loan_id_required);
+        void setMerchantIdRequired(bool merchant_id_required);
+        void setName(const string& name);
+        void setLegRequired(bool leg_required);
         // void setVariableName(const string& value) { variable_name = value; }
 
         //GETTERS
-        bool getCashierIdRequired() const { return cashier_id_required; }
-        const vector<int>& getCreditAvailableIds() const { return credit_available_ids; }
-        int getCreditBondIdRequired() const { return credit_bond_id_required; }
-        bool getCustomerIdRequired() const { return customer_id_required; }
-        const vector<int>& getDebitAvailableIds() const { return debit_available_ids; }
-        int getDebitBondIdRequired() const { return debit_bond_id_required; }
-        int getId() const { return id; }
-        bool getInstallmentIdRequired() const { return installment_id_required; }
-        bool getLoanIdRequired() const { return loan_id_required; }
-        bool getMerchantIdRequired() const { return merchant_id_required; }
-        const string& getName() const { return name; }
-        bool getLegRequired() const { return leg_required; }
+        const bool getCashierIdRequired();
+        const vector<int>& getCreditAvailableIds();
+        const int getCreditBondIdRequired();
+        const bool getCustomerIdRequired();
+        const vector<int>& getDebitAvailableIds();
+        const int getDebitBondIdRequired();
+        const int getId();
+        const bool getInstallmentIdRequired();
+        const bool getLoanIdRequired();
+        const bool getMerchantIdRequired();
+        const string& getName();
+        const bool getLegRequired();
         // const string& getVariableName() const { return variable_name; }        
         ledger_amount_primitive_orm * get_ledger_amount_primitive_orm();
-        ~TemplateLeg(){} 
+        ~TemplateLeg();
 };
 
 class LedgerCompositLeg
