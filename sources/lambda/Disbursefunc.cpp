3de449d952c5a62fffc9aa736f7903c346044345
Abdallah Gomaa <abdallah.gomaa@blnk.ai>
2024-05-20 17:14:30 +0300

    fixed indentation

--- sources/lambda/Disbursefunc.cpp
+++ sources/lambda/Disbursefunc.cpp
@@ -2,24 +2,24 @@
 
 void DisburseLoanFunc (map<string, PSQLAbstractORM*>* orms, int partition_number, mutex* shared_lock,void * extras) {
 
-        BlnkTemplateManager* localTemplateManager = new BlnkTemplateManager(((DisburseLoanStruct *) extras)->blnkTemplateManager);
-        DisburseLoan disburseLoan(orms, ((DisburseLoanStruct *) extras)->current_provision_percentage);
-        LedgerClosureService* ledgerClosureService = new LedgerClosureService(&disburseLoan);
+    BlnkTemplateManager* localTemplateManager = new BlnkTemplateManager(((DisburseLoanStruct *) extras)->blnkTemplateManager);
+    DisburseLoan disburseLoan(orms, ((DisburseLoanStruct *) extras)->current_provision_percentage);
+    LedgerClosureService* ledgerClosureService = new LedgerClosureService(&disburseLoan);
 
-        disburseLoan.setupLedgerClosureService(ledgerClosureService);
-        map<string, LedgerAmount*>* ledgerAmounts = ledgerClosureService->inference();
-        localTemplateManager->setEntryData(ledgerAmounts);
+    disburseLoan.setupLedgerClosureService(ledgerClosureService);
+    map<string, LedgerAmount*>* ledgerAmounts = ledgerClosureService->inference();
+    localTemplateManager->setEntryData(ledgerAmounts);
 
-        ledger_entry_primitive_orm* entry = localTemplateManager->buildEntry(BDate("2024-05-15"));
-        ledger_amount_primitive_orm* la_orm = localTemplateManager->getFirstLedgerAmountORM();
+    ledger_entry_primitive_orm* entry = localTemplateManager->buildEntry(BDate("2024-05-15"));
+    ledger_amount_primitive_orm* la_orm = localTemplateManager->getFirstLedgerAmountORM();
 
-        if (entry && la_orm) {
-            disburseLoan.stampORMs(entry, la_orm);
-        } else {
-            cerr << "Cannot stamp ORM objects\n";
-            exit(1);
-        }
+    if (entry && la_orm) {
+        disburseLoan.stampORMs(entry, la_orm);
+    } else {
+        cerr << "Cannot stamp ORM objects\n";
+        exit(1);
+    }
 
-        delete localTemplateManager;
-        delete ledgerClosureService;
-    };
+    delete localTemplateManager;
+    delete ledgerClosureService;
+};
