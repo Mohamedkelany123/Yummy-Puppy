99dfdafea1323d32fa9fea0670436e1d2cbf1b38
Karim Mohamed Sobh <omneya.hamroush@blnk.ai>
2024-05-21 19:03:12 +0300

    fix in queries aggregator

--- sources/mains/cmd_services/interest_accrual.cpp
+++ sources/mains/cmd_services/interest_accrual.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
 
     int threads_count = 1;
 
-    bool connect = psqlController.addDataSource("main","192.168.65.216",5432,"django_ostaz_30042024_omneya","postgres","8ZozYD6DhNJgW7a");
+    bool connect = psqlController.addDataSource("main","192.168.65.216",5432,"ledger_closure_omneya","postgres","8ZozYD6DhNJgW7a");
     if (connect){
         cout << "Connected to DATABASE"  << endl;
     }
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     {new loan_app_loan_primitive_orm("main"),new loan_app_installment_primitive_orm("main"), new new_lms_installmentextension_primitive_orm("main")},
     {{{"loan_app_loan","id"},{"loan_app_installment","loan_id"}}, {{"loan_app_installment", "id"}, {"new_lms_installmentextension", "installment_ptr_id"}}});
 
-    string closure_date_string = "2024-05-15"; 
+    string closure_date_string = "2024-05-20"; 
 
     accrualQuery->addExtraFromField("(select day from loan_app_loanstatushistroy lalsh  where loan_app_loan.id=lalsh.loan_id and status_type =0 and previous_status_id =loan_app_loan.marginalization_bucket_id and lalsh.status_id>loan_app_loan.marginalization_bucket_id and day <= new_lms_installmentextension.partial_accrual_date and status_id not in (6,7,8,12,13,14,15,16) order by id desc limit 1)","marginalization_history");
     accrualQuery->addExtraFromField("(select paid_at from payments_loanorder plo where plo.status=1 AND loan_app_loan.id=plo.loan_id order by paid_at desc limit 1)","last_order_date");
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
     
     accrualQuery->filter(
         ANDOperator (
-            new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::INTEREST_ACCRUAL-1),
+            // new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::INTEREST_ACCRUAL-1),
             new OROperator (
                 new UnaryOperator("new_lms_installmentextension.accrual_date", lte, closure_date_string),
                 new ANDOperator(
@@ -51,7 +51,7 @@ int main(int argc, char** argv) {
     );
 
     accrualQuery->setOrderBy("loan_app_loan.id");
-
+    
     BlnkTemplateManager * accrualTemplateManager = new BlnkTemplateManager(8);
 
     AccrualInterestStruct accrualInterestStruct = {
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     partialAccrualQuery->filter(
         ANDOperator (
             new UnaryOperator("new_lms_installmentextension.partial_accrual_date", lte, closure_date_string),
-            new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::PARTIAL_INTEREST_ACCRUAL-1),
+            // new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::PARTIAL_INTEREST_ACCRUAL-1),
             new UnaryOperator("new_lms_installmentextension.partial_accrual_ledger_amount_id", isnull, "", true),
             new UnaryOperator("new_lms_installmentextension.expected_partial_accrual_amount", ne, 0),
             new UnaryOperator("new_lms_installmentextension.settlement_accrual_interest_date", isnull, "", true),
@@ -84,8 +84,8 @@ int main(int argc, char** argv) {
                 )
             ),
             new UnaryOperator("new_lms_installmentextension.status_id", nin, "12, 13"),
-            new UnaryOperator("new_lms_installmentextension.interest_expected", ne, 0),
-            new UnaryOperator("new_lms_installmentextension.partial_accrual_date", ne, "new_lms_installmentextension.accrual_date")
+            new UnaryOperator("loan_app_installment.interest_expected", ne, 0),
+            new UnaryOperator("new_lms_installmentextension.partial_accrual_date", ne, "new_lms_installmentextension.accrual_date", true)
         )
     );
     partialAccrualQuery->setOrderBy("loan_app_loan.id");
@@ -108,12 +108,12 @@ int main(int argc, char** argv) {
     settlementAccrualQuery->filter(
         ANDOperator (
             new UnaryOperator("new_lms_installmentextension.settlement_accrual_interest_date", lte, closure_date_string),
-            new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::SETTLEMENT_INTEREST_ACCRUAL-1),
+            // new UnaryOperator("loan_app_loan.closure_status", eq, ledger_status::SETTLEMENT_INTEREST_ACCRUAL-1),
             new UnaryOperator("new_lms_installmentextension.settlement_accrual_interest_ledger_amount_id", isnull, "", true),
-            new UnaryOperator("new_lms_installmentextension.status", nin, "12, 13"),
+            new UnaryOperator("new_lms_installmentextension.status_id", nin, "12, 13"),
             new UnaryOperator("new_lms_installmentextension.settlement_accrual_interest_amount", ne, 0),
             new UnaryOperator("loan_app_loan.status_id", nin, "12, 13"),
-            new UnaryOperator("new_lms_installmentextension.interest_expected", ne, 0)
+            new UnaryOperator("loan_app_installment.interest_expected", ne, 0)
         )
     );
 
