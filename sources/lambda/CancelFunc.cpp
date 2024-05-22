354bc48b13177c20ae3e0e7241de968fd7ea8e41
Alia Saddik <alia.saddik@blnk.ai>
2024-05-22 11:24:54 +0300

    one entry

--- sources/lambda/CancelFunc.cpp
+++ sources/lambda/CancelFunc.cpp
@@ -8,11 +8,10 @@ void CancelLoanFunc (loan_app_loan_bl_orm * _lal_orm, int partition_number, mute
 
     if (is_included=="t"){
 
-        ledger_entry_primitive_orm* entry = new ledger_entry_primitive_orm("main");
         BlnkTemplateManager* localTemplateManager = new BlnkTemplateManager(((CancelLoanStruct *) extras)->blnkTemplateManager_reverse);
         
-        vector <ledger_amount_primitive_orm*>* la_orms = _lal_orm->get_ledger_amount_loan_id() ;       
-        entry = localTemplateManager->reverseEntry(la_orms,BDate("2024-05-15"));
+        vector <ledger_amount_primitive_orm*>* la_orms = _lal_orm->get_ledger_amount_loan_id(true) ;       
+        ledger_entry_primitive_orm* entry = localTemplateManager->reverseEntry(la_orms,BDate("2024-05-15"));
 
         if (_lal_orm->get_loan_upfront_fee()>0 & !_lal_orm->get_refund_upfront_fee_bool()){
             BlnkTemplateManager* localTemplateManager_cancel = new BlnkTemplateManager(((CancelLoanStruct *) extras)->blnkTemplateManager_cancel);
