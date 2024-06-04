0c5c114245a789165f4f815a8902899f57b75166
Abdallah Gomaa <abdallah.gomaa@blnk.ai>
2024-06-04 11:57:12 +0300

    implemented writing serialized data to stream

--- headers/test_serializer/TestSerializer.h
+++ headers/test_serializer/TestSerializer.h
@@ -2,17 +2,21 @@
 #define TESTSERIALIZER_H
 
 #include <PSQLAbstractQueryIterator.h>
+#include <fstream>
 
 
 
 class TestSerializer {
 
     private:
-        
+        string store;
+        PSQLJoinQueryIterator * psqlQueryJoin;
+        bool write_to_stream(string orm_serialized);
+        bool commit_to_stream ();
         bool generate_simple();
         bool generate_aggregate();
     public:
-        TestSerializer(string store_location,PSQLJoinQueryIterator * psqlQueryJoin);
+        TestSerializer(string store_location,PSQLJoinQueryIterator * _psqlQueryJoin);
         bool generate();
         ~TestSerializer();
 
