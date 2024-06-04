62597bfc13a9060425a9dac30b72e90e342027a6
Karim Mohamed Sobh <omneya.hamroush@blnk.ai>
2024-06-04 12:36:42 +0300

    added changes to serializer files

--- headers/test_serializer/TestSerializer.h
+++ headers/test_serializer/TestSerializer.h
@@ -9,15 +9,15 @@
 class TestSerializer {
 
     private:
-        string store;
+        string store_location;
         PSQLJoinQueryIterator * psqlQueryJoin;
         bool write_to_stream(string orm_serialized);
         bool commit_to_stream ();
         bool generate_simple();
         bool generate_aggregate();
     public:
-        TestSerializer(string store_location,PSQLJoinQueryIterator * _psqlQueryJoin);
-        bool generate();
+        TestSerializer(string _store_location,PSQLJoinQueryIterator * _psqlQueryJoin);
+        bool generate(is_aggregate);
         ~TestSerializer();
 
 };
--- sources/test_serializer/TestSerializer.cpp
+++ sources/test_serializer/TestSerializer.cpp
@@ -1,9 +1,48 @@
 #include "TestSerializer.h"
 
-TestSerializer::TestSerializer(string store_location, PSQLJoinQueryIterator *_psqlQueryJoin)
+
+
+
+TestSerializer::TestSerializer(string  _store_location, PSQLJoinQueryIterator * _psqlQueryJoin)
 {
-    this->store = store_location;
+    store_location = _store_location;
+    psqlQueryJoin = _psqlQueryJoin;
+   
 }
+
+
+
+bool TestSerializer::generate_simple()
+{
+   
+    return false
+}
+
+bool TestSerializer::generate_aggregate()
+{
+   
+    return false
+}
+
+
+bool TestSerializer::commit_to_stream()
+{
+   
+    return false
+}
+
+bool TestSerializer::generate(bool is_aggregate)
+{
+   if (is_aggregate)
+    this->generate_aggregate();
+   else
+    this->generate_simple(); 
+   
+
+}
+
+
+
 
 bool TestSerializer::write_to_stream(string orm_serialized)
 {
