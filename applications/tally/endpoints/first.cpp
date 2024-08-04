d0d7d0d1e3cd8d25eb198d34a5af5287eb35670d
Karim Sobh <kmsobh@blnk.ai>
2024-08-04 18:25:48 +0300

    First Tally Push

--- applications/tally/endpoints/first.cpp
+++ applications/tally/endpoints/first.cpp
@@ -2,7 +2,7 @@
 #include <ThorSerialize/SerUtil.h>
 #include <ThorSerialize/JsonThor.h>
 #include <EndpointService.h>
-#include <loan_app_loan_primitive_orm.h>
+// #include <loan_app_loan_primitive_orm.h>
 
 class AbstractSerializer
 {
@@ -133,67 +133,15 @@ class FawryInquiryResponse: public AbstractSerializer
 
 ThorsAnvil_MakeTrait(FawryInquiryResponse,postFawryInquiryOutput);
 
-// template <class I,class O>
-// string endpoint_entry(string http_body,std::function<void(string http_body,I * inputSerializer,O * outputSerializer)> f) {
 
-//         I * inputSerializer  = new I(); 
-//         O * outputSerializer  = new O(); 
-
-//         inputSerializer->serialize(http_body);
-//         f (http_body,inputSerializer,outputSerializer);
-//         string str_return = outputSerializer->deserialize();
-//         delete (inputSerializer);
-//         delete (outputSerializer);
-//         return str_return;
-// }
-
-
-// void serve_endpoint (char * http_request,char * http_reply)
-// {
-//     string str = endpoint_entry <FawryInquiry,FawryInquiryResponse> (http_request,[] (string http_body,FawryInquiry * inputSerializer,FawryInquiryResponse * outputSerializer) {
-
-//         cout << inputSerializer->getPOSTFawryInquiryInput().getMSGCode() << "\n";
-//         cout << inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].getKey() << "\n";
-//         cout << inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].getValue() << "\n";
-//         ((FawryInquiry *)inputSerializer)->getPOSTFawryInquiryInput().getCustomProperties()[0].setValue("01001091779");
-//         // jsonData.transactions.transaction[1].payment_id = 100;
-//         outputSerializer->getPOSTFawryInquiryOutput().setMSGCode("Hello all");
-//     });
-//     strcpy (http_reply,str.c_str());
-// }
-
-
-// std::function<void(string http_body,FawryInquiry * inputSerializer,FawryInquiryResponse * outputSerializer)> l = [] (string http_body,FawryInquiry * inputSerializer,FawryInquiryResponse * outputSerializer) {
-//     cout << inputSerializer->getPOSTFawryInquiryInput().getMSGCode() << "\n";
-//     cout << inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].getKey() << "\n";
-//     cout << inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].getValue() << "\n";
-//     inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].setValue("01001091779");
-//     // jsonData.transactions.transaction[1].payment_id = 100;
-//     outputSerializer->getPOSTFawryInquiryOutput().setMSGCode("Hello all from serializer"+inputSerializer->getPOSTFawryInquiryInput().getMSGCode());
-// };
 
 extern "C" HTTPService *create_object() // extern "c" not garbling function names
 {       
-    bool connect = psqlController.addDataSource("main","localhost",5432,"django_ostaz_08072024","postgres","postgres");
-    if (connect){
-        cout << "Connected to DATABASE"  << endl;
-    }
-    int threadsCount = 1;
-    psqlController.addDefault("created_at","now()",true,true);
-    psqlController.addDefault("updated_at","now()",true,true);
-    psqlController.addDefault("updated_at","now()",false,true);
-    psqlController.setORMCacheThreads(threadsCount);
+   
 
     return new EndpointService <FawryInquiry,FawryInquiryResponse>(
             [] (string http_body,FawryInquiry * inputSerializer,FawryInquiryResponse * outputSerializer) {
 
-
-            ORMVector <loan_app_loan_primitive_orm> ormVector = loan_app_loan_primitive_orm::fetch("main",UnaryOperator ("id",lte,100));
-            for ( int i  = 0;  i < ormVector.size() ; i ++)
-            {
-                cout << ormVector[i]->get_id() << " " << ormVector[i]->get_principle() << endl;
-
-            }
 
             cout << inputSerializer->getPOSTFawryInquiryInput().getMSGCode() << "\n";
             cout << inputSerializer->getPOSTFawryInquiryInput().getCustomProperties()[0].getKey() << "\n";
--- headers/endpoint/EndpointService.h
+++ headers/endpoint/EndpointService.h
@@ -1,7 +1,4 @@
 #include <PSQLController.h>
-#include <TemplateManager.h>
-#include <DisburseLoans.h>
-#include <common_orm.h>
 #include <common.h>
 #include <PSQLAbstractQueryIterator.h>
 #include <HTTPService.h>
