6d14e9a58e8a767e796c5df5bc51904733b58e0e
ramy-blnk <ramy.ahmed@blnk.ai>
2024-08-14 22:39:45 +0300

    Fix Documentation location

--- Webserver-Backend/sources/libs/HTTPResponseHeader.cpp
+++ Webserver-Backend/sources/libs/HTTPResponseHeader.cpp
@@ -1,21 +1,30 @@
 
 #include "HTTPResponseHeader.h"
 // Constructor that sets data members using initialization list. status, status_code, and protocol are used to set up the first line of the header
-HTTPResponseHeader::HTTPResponseHeader(string p_status,int p_status_code,string p_protocol):
-status(p_status),status_code(p_status_code),protocol(p_protocol)
+HTTPResponseHeader::HTTPResponseHeader(string p_status, int p_status_code, string p_protocol) : status(p_status), status_code(p_status_code), protocol(p_protocol)
 {
-    
 }
 // Add a new response header "key: value"
-void HTTPResponseHeader::setHeader(string p_key,string p_value)
+void HTTPResponseHeader::setHeader(string p_key, string p_value)
 {
-    header[p_key]=p_value; // add to the header map
+    header[p_key] = p_value; // add to the header map
 }
 
-
-
 // Build up the HTTP header from the header map and write it on the socket back to the client
-void HTTPResponseHeader::respond (TCPSocket * tcpSocket)
+/**
+ * @brief This function builds up the HTTP header from the header map and writes it on the socket back to the client.
+ *
+ * @param tcpSocket A pointer to the TCPSocket object representing the client's connection.
+ *
+ * The function first composes the first line of the header using the protocol, status code, and status.
+ * Then, it iterates through the header map using a for_each loop with a lambda function to build up the rest of the header.
+ * Each key-value pair in the header map is added to the header string in the format "key: value".
+ * After building up the entire header, an empty line is added as the end of header indicator.
+ * Finally, the header string is written to the client over the tcpSocket using the writeToSocket method.
+ * @author kmsobh
+ * @date 10-Jun-2024
+ */
+void HTTPResponseHeader::respond(TCPSocket *tcpSocket)
 {
     // Compose the first line in the header
     string header_string = protocol;
@@ -25,16 +34,16 @@ void HTTPResponseHeader::respond (TCPSocket * tcpSocket)
     header_string += status;
     header_string += "\r\n"; // first line delimiter
     // A for_each iterator base loop with lambda function for building up the rest of the header
-    for_each (header.begin(),header.end(),[&header_string](const std::pair<string,string>& it) -> bool {
+    for_each(header.begin(), header.end(), [&header_string](const std::pair<string, string> &it) -> bool
+             {
             header_string +=  std::get<0>(it); 
             header_string += ": " ;
             header_string += std::get<1>(it);
             header_string += "\r\n";
-            return true; 
-    });
+            return true; });
     header_string += "\r\n"; // Empty line as end of header indicator
     // Write header to client over tcpSocket
-    tcpSocket->writeToSocket(header_string.c_str(),header_string.length());
+    tcpSocket->writeToSocket(header_string.c_str(), header_string.length());
 }
 void HTTPResponseHeader::setStatus(string p_status)
 {
@@ -49,4 +58,4 @@ void HTTPResponseHeader::setProtocol(string p_protocol)
     protocol = p_protocol;
 }
 // Destructor
-HTTPResponseHeader::~HTTPResponseHeader(){}
+HTTPResponseHeader::~HTTPResponseHeader() {}
