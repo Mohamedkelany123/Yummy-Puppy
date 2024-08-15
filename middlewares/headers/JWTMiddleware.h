63668b856e7185f1414de3c4801f433fd5d679df
Karim Sobh <kmsobh@blnk.ai>
2024-08-15 18:13:45 +0300

    JWT connection ton database and unpacking JWT payload beta version

--- middlewares/headers/JWTMiddleware.h
+++ middlewares/headers/JWTMiddleware.h
@@ -1,7 +1,11 @@
 #ifndef _JWTMIDDLEWARE_H_
 #define _JWTMIDDLEWARE_H_
-#include "Middleware.h"
+#include <Middleware.h>
+#include <Poco/JWT/JWT.h>
+#include <Poco/JWT/Signer.h>
+#include <Poco/JWT/Token.h>
 
+using namespace Poco::JWT;
 
 
 class  JWTMiddleware: public Middleware
