2f7ab0060c100177638f5f0310faab39d0978e49
ramy-blnk <ramy.ahmed@blnk.ai>
2024-08-21 18:26:59 +0300

    Update Common.h Macro StartEndpoint

--- headers/common.h
+++ headers/common.h
@@ -73,7 +73,7 @@ using json = nlohmann::json;
 
 
 #define STARTPOSTENDPOINT(I,O) return new EndpointService <I,O>( \
-            [] (string http_body,I * inputSerializer,O * outputSerializer) {
+            [] (HTTPRequest HTTPRequest,I * inputSerializer,O * outputSerializer) {
 
 
 #define ENDPOSTENDPOINT });
