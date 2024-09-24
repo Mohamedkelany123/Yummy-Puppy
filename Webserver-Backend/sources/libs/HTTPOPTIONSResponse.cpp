9e1ebbae693b16e58ec1bc2d94d159a19ca89810
Ahmed Asem <ahmed.asem@blnk.ai>
2024-09-24 15:30:35 +0300

    making Access-Control-Allow-Methods allow all

--- Webserver-Backend/sources/libs/HTTPOPTIONSResponse.cpp
+++ Webserver-Backend/sources/libs/HTTPOPTIONSResponse.cpp
@@ -3,7 +3,7 @@
 // Constructor: Calling the base class constructor
 HTTPOPTIONSResponse::HTTPOPTIONSResponse(TCPSocket * _tcpSocket) : HTTPResponse(_tcpSocket){
     this->setHeaderValue("Access-Control-Allow-Origin", "*");
-    this->setHeaderValue("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    this->setHeaderValue("Access-Control-Allow-Methods", "*");
     this->setHeaderValue("Access-Control-Allow-Headers", "Content-Type");
 }
 
