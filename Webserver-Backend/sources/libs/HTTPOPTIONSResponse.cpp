fd45edcd328706cece2521c7866512a620cb2b9a
Ahmed Asem <ahmed.asem@blnk.ai>
2024-09-24 15:37:38 +0300

    adding Access-control-allow-credentials

--- Webserver-Backend/sources/libs/HTTPOPTIONSResponse.cpp
+++ Webserver-Backend/sources/libs/HTTPOPTIONSResponse.cpp
@@ -4,6 +4,7 @@
 HTTPOPTIONSResponse::HTTPOPTIONSResponse(TCPSocket * _tcpSocket) : HTTPResponse(_tcpSocket){
     this->setHeaderValue("Access-Control-Allow-Origin", "*");
     this->setHeaderValue("Access-Control-Allow-Methods", "*");
+    this->setHeaderValue("Access-control-allow-credentials", "true");
     this->setHeaderValue("Access-Control-Allow-Headers", "Content-Type");
 }
 
